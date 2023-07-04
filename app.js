17cc90d740e455b4f93a1acc50a769cfdae9f1c3
Prakhar Sinha <68687087+PrakharSinha-09@users.noreply.github.com>
2023-07-04 17:11:50 +0000

    fixed some typos

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 require('dotenv').config()
+require('./database/database').connect()
 const express=require('express')
-
+import Users from './models/users'
 const app=express()
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send("<h1>Server is Working!</h1>")
 })
+
+app.post('/register',async (req,res)=>{
+    try{
+        //get all data from the body.
+        //we will be checking if the user already exists
+        //encrypting password
+        //save the user in DB
+        //generating token for the user and then send it
+        //in the login, we have to store these token in the cookie of the user
+        //and the best way to get this is to send the sevrer side cookies right
+
+        const {firstName,lastName,email,password}=req.body
+        
+
+
+    }
+    catch(error){
+        res.json({"error":"Something wrong Occured!"})
+    }
+
+})
+
 module.exports=app
--- database/database.js
+++ database/database.js
@@ -1,15 +1,18 @@
 const mongoose=require("mongoose")
-const MONGODB_URL=process.env
+const {MONGODB_URL}=process.env
 
 exports.connect=()=>{
+    // console.log(typeof MONGODB_URL)
     mongoose.connect(MONGODB_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
     })
-    .then()
+    .then(()=>{
+        console.log('MongoDB Connected Successfully!')
+    })
     .catch((error)=>{
         console.log('DB Connection FAILED');
-        console.log(console.error(););
+        console.log(error);
         process.exit(1)
     })
 }
